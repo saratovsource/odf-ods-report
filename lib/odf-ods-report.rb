c56dab63ae4d07e7c3de8fa4207200f1b1c32e23
Alexander Kirillov <saratovsource@gmail.com>
2015-10-01 19:14:15 +0300

    Update odf-ods-report.rb

--- lib/odf-ods-report.rb
+++ lib/odf-ods-report.rb
@@ -4,4 +4,4 @@ require 'fileutils'
 require 'nokogiri'
 
 require File.expand_path('../odf-report',  __FILE__)
-require File.expand_path('../ods_report',  __FILE__)
+require File.expand_path('../ods-report',  __FILE__)
